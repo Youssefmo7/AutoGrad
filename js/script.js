799a06b40df1d225396863e51488616922929f40
ibrahemomara <hemaomara11@gmail.com>
2025-04-20 21:28:44 +0200

    ibrahem update add doc and std btn

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ registerbtn.addEventListener('click', ()=>{
 loginbtn.addEventListener('click', ()=>{
     container.classList.remove('active');
 });
+
+
+
+let selectedRole = "null";
+
+function selectRole(role) {
+selectedRole = role;
+document.getElementById("studentBtn").classList.remove("active");
+document.getElementById("doctorBtn").classList.remove("active");
+
+if (role === "student") {
+document.getElementById("studentBtn").classList.add("active");
+} else {
+document.getElementById("doctorBtn").classList.add("active");
+}
+}
+
+function handleFormSubmit(e) {
+e.preventDefault();
+
+if (!selectedRole) {
+alert("من فضلك اختر طالب أو دكتور");
+return false;
+}
+
+// هنا بقى تقدر تتعامل مع الفورم زي ما تحب
+if (selectedRole === "student") {
+window.location.href = "/studentprofile.html";
+} else if (selectedRole === "doctor") {
+window.location.href = "/DoctorProfile.html";
+}
+
+return false;
+}
+
